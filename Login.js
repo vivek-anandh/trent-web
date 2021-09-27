60e69b71da88d3df2a033fe8a5ce65762ff62a93
vivek-anandh <u.vivekanandh@gmail.com>
2021-09-27 09:01:03 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -61,7 +61,7 @@ e.read=function(t,e,r,n,i){var o,s,a=8*i-n-1,u=(1<<a)-1,c=u>>1,f=-7,h=r?i-1:0,l=
 	var initAuthFlow = function(){		
 		var url = window.location.href;
 		if(url.indexOf('?') != -1) {
-			url = url(0, url('?')); 	
+			url = url.substring(0, url.index.Of('?')); 	
 		}
 		var settings = {};
 		settings.client_id = "2tegq8gghd0donvoa9q622e0n5";
