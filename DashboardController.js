060dcfdf6a6822038f4076790df2fea266ca74b2
Vivek Anandh <u.vivekanandh@gmail.com>
2025-06-30 21:04:29 +0530

    Fund maintanance (#1)
    * Delete PortfolioManager.js
    
    * Create DashboardController.js
    
    * fund logic

--- DashboardController.js
+++ DashboardController.js
@@ -1,3 +1,169 @@
+app.controller('TrentAngularAppController', ['$scope', '$filter', '$q', function($scope, $filter, $q) {
+
+$scope.numberClass	= function(value) {
+	if(value){
+		value = Number(value);
+		return value >=0 ? "text-success" : "text-danger";
+	}
+	return "";
+};
+
+
+$scope.data	= {};
+
+$scope.loadFundsDataPromise = function(allFundsData) {
+	var deferred = $q.defer();
+	initaializePortfolioPromise(allFundsData).then((response) => {
+		deferred.resolve(response);
+	});
+	return deferred.promise;
+};
+
+$scope.loadFundsData = function(allFundsData) {
+	var promise = $scope.loadFundsDataPromise(allFundsData);
+	promise.then(function(response) {
+		$scope.data = response;
+		console.log(response)
+	});		
+};
+
+$scope.getAllDataPromise = function(token) {
+
+	return new Promise(function (resolve, reject) {
+		try {
+			// Creating the XMLHttpRequest object
+			var request = new XMLHttpRequest();
+			
+			var api = "api.json";
+			if(prodMode){
+				api  = "https://8ecbjv99ca.execute-api.ap-south-1.amazonaws.com/UAT/holdings"; 					
+			}
+			request.open("GET", api);
+			
+			if(prodMode){
+				request.setRequestHeader("Authorization", "Bearer " + token);
+				request.setRequestHeader("X-API-KEY", "CcVLdqEx0s2MR4bsnnIQ19p4mMc1a5ai48HuwZVD")
+			}
+			// Defining event listener for readystatechange event
+			request.onreadystatechange = function() {
+				// Check if the request is compete and was successful
+				if(this.readyState === 4 && this.status === 200) {
+					// Inserting the response from server into an HTML element
+					resolve(this.responseText);
+				}
+			};
+
+			// Sending the request to the server
+			request.send();
+			
+		} catch (e) {
+			console.error(e);
+			reject('Error in API Call');
+		}
+	});		
+
+};
+$scope.getAllData = function(token) {
+	var promise = $scope.getAllDataPromise(token);
+	promise.then(function(response) {
+		var json = JSON.parse(response);
+		$scope.loadFundsData(json.body ? json.body : json);
+		});		
+};
+
+$scope.signInAndLoadScreen = function () {
+	if(prodMode) {
+		initAuthFlow().then(function(response) {
+			$scope.getAllData(response.access_token);
+		});	
+	}
+	else{
+		$scope.getAllData();
+	}
+};
+
+$scope.signInAndLoadScreen();
+
+
+$scope.formatDate = function (date) {
+	if (!date) {
+		return '';
+	}
+	let easyDay = $scope.getEasyDay(date);
+	if (easyDay) {
+		return 'NAV Date : ' + easyDay;
+	}
+	return (
+		'NAV Date : ' + 
+		date.getDate() +
+		'/' +
+		$scope.getMonth(date.getMonth()) +
+		'/' +
+		(Number(date.getFullYear()) - 2000)
+	);
+};
+
+$scope.getEasyDay = function(date) {
+	let today = new Date();
+	if (
+		today.getFullYear() === date.getFullYear() &&
+		today.getMonth() === date.getMonth()
+	) {
+		if (today.getDate() === date.getDate()) {
+			return 'Today';
+		}
+		if (today.getDate() - 1 === date.getDate()) {
+			return 'Yesterday';
+		}
+		if (today.getDate() - 2 === date.getDate()) {
+			return '2 Days ago';
+		}
+		if (today.getDate() - 3 === date.getDate()) {
+			return '3 Days ago';
+		}
+	}
+	return '';
+};
+
+$scope.getMonth = function (month) {
+	let monthStr = '';
+	switch (month) {
+		case 0:
+			return 'Jan';
+		case 1:
+			return 'Feb';
+		case 2:
+			return 'Mar';
+		case 3:
+			return 'Apr';
+		case 4:
+			return 'May';
+		case 5:
+			return 'Jun';
+		case 6:
+			return 'Jul';
+		case 7:
+			return 'Aug';
+		case 8:
+			return 'Sep';
+		case 9:
+			return 'Oct';
+		case 10:
+			return 'Nov';
+		case 11:
+			return 'Dec';
+	}
+	return monthStr;
+};
+
+$scope.showFundDtl = function (fund){
+	$scope.fundDtl = fund;
+};
+
+
+}]);
+
+
 
 // for deep-lining urls to pages, use linking in NavigationContainer
 function initaializePortfolioPromise(data) {
---
+++ app.js
@@ -0,0 +1,284 @@
+var prodMode = true;
+var accessToken  = 'eyJraWQiOiI2VkhPbnRRS3I5a2tsYjZHRTlseWFKUXlFZjVOdm9yTG9zRFRIYU1MSHVjPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIxYjcwODk4NC0wOWFkLTQ2OGEtYTU4NS0xMDNhNzQ2YzdhODAiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCIsImF1dGhfdGltZSI6MTc1MTI4MDU3OCwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLXNvdXRoLTEuYW1hem9uYXdzLmNvbVwvYXAtc291dGgtMV9CSnRMUlEzMDYiLCJleHAiOjE3NTEzNjY5MTgsImlhdCI6MTc1MTI4MDU3OCwidmVyc2lvbiI6MiwianRpIjoiZjg3MzE5MzQtMjMwNC00YWUzLWE1NmMtYzNkMWZhODA0ZGUwIiwiY2xpZW50X2lkIjoiMnRlZ3E4Z2doZDBkb252b2E5cTYyMmUwbjUiLCJ1c2VybmFtZSI6InZpdmVrIn0.pt96yXDLk9Sch8ssgqJX-h2hBZKHNESx2jYT8CL8-afnQiaRUSy7C0JTZTCOIZOYFOcwcH2mvAH1xx61aR8XuPu6BcoQzlV8_djQqHXmABFF6gNGEbQ49akzN633prY6TT-wXplu-keLCXGgvyu7kvKZqHIJ_sPX1gcmcuZ-y0_Z02bsY4Ff1xqTtvrAh26MpOKr5sb4sOyh9PLzQXTs3oxFHSVrYdL-kBNkYF05cUPJ0aQrUaKPVVtUoNegsVyZGTQit9_YVBzR0nz3X2gmKk1g-eB5g4mqoEIqBNcq9f9T5ZS_x58BLZMFuweoWI1dBwEOgKMojOgO874FNvzQaw';	
+
+// TrentAngularAppController.js or new app.js
+var app = angular.module('TrentAngularApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'TrentAngularAppController'
+        })
+        .when('/maintain-book', {
+            templateUrl: 'maintain-book.html',
+            controller: 'MaintainBookController'
+        })
+        .when('/maintain-fund', {
+            templateUrl: 'maintain-fund.html',
+            controller: 'MaintainFundController'
+        })
+        .otherwise({redirectTo: '/'});
+}]);
+
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.common['Authorization'] = 'Bearer ' + accessToken;
+    $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
+}]);
+
+app.run(['$rootScope', '$location', function($rootScope, $location) {
+    $rootScope.isActive = function(route) {
+        return route === $location.path();
+    };
+
+    $rootScope.setActive = function(route) {
+        $location.path(route);
+    };
+}]);
+
+app.controller('MaintainBookController', ['$scope', '$http', '$q', function($scope, $http, $q) {
+    $scope.books = [];
+    $scope.filter = {
+        fundId: '',
+        date: ''
+    };
+
+    // 15 sample books for pagination demo
+    var localBooks = [
+        { fundId: 101, quantity: 10.5, price: 100.25, date: "2025-06-25" },
+        { fundId: 102, quantity: -5.0, price: 200.00, date: "2025-06-24" },
+        { fundId: 101, quantity: 7.25, price: 150.75, date: "2025-06-23" },
+        { fundId: 103, quantity: 12.0, price: 110.00, date: "2025-06-22" },
+        { fundId: 104, quantity: 8.5, price: 120.50, date: "2025-06-21" },
+        { fundId: 105, quantity: 15.0, price: 130.00, date: "2025-06-20" },
+        { fundId: 106, quantity: 9.0, price: 140.75, date: "2025-06-19" },
+        { fundId: 107, quantity: 11.5, price: 150.25, date: "2025-06-18" },
+        { fundId: 108, quantity: 13.0, price: 160.00, date: "2025-06-17" },
+        { fundId: 109, quantity: 14.5, price: 170.50, date: "2025-06-16" },
+        { fundId: 110, quantity: 16.0, price: 180.00, date: "2025-06-15" },
+        { fundId: 111, quantity: 17.5, price: 190.25, date: "2025-06-14" },
+        { fundId: 112, quantity: 18.0, price: 200.00, date: "2025-06-13" },
+        { fundId: 113, quantity: 19.5, price: 210.75, date: "2025-06-12" },
+        { fundId: 114, quantity: 20.0, price: 220.00, date: "2025-06-11" }
+    ];
+
+    // Pagination variables
+    $scope.currentPage = 1;
+    $scope.pageSize = 5;
+
+    // Fetch books
+    $scope.loadBooks = function () {
+        if (window.prodMode) {
+            $http.get("https://8ecbjv99ca.execute-api.ap-south-1.amazonaws.com/UAT/book", {
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(function (response) {
+                // If API returns { body: [...] }
+                $scope.books = response.data.body ? response.data.body : response.data;
+            }, function (error) {
+                alert("Failed to load books from API.");
+                $scope.books = [];
+            });
+        } else {
+            $scope.books = angular.copy(localBooks);
+        }
+    };
+
+    // Filter function for ng-repeat
+    $scope.bookFilter = function(book) {
+        var match = true;
+        if ($scope.filter.fundId) {
+            match = match && (book.fundId == $scope.filter.fundId);
+        }
+        if ($scope.filter.date) {
+            // Convert JS Date object to YYYY-MM-DD string
+            var d = $scope.filter.date;
+            var dateStr = (d instanceof Date)
+                ? d.getFullYear() + '-' +
+                  ('0' + (d.getMonth() + 1)).slice(-2) + '-' +
+                  ('0' + d.getDate()).slice(-2)
+                : $scope.filter.date;
+            match = match && (book.date == dateStr);
+        }
+        return match;
+    };
+
+    $scope.clearFilter = function() {
+        $scope.filter.fundId = '';
+        $scope.filter.date = '';
+    };
+
+    // Pagination helpers
+    $scope.getPagedBooks = function(filteredBooks) {
+        var start = ($scope.currentPage - 1) * $scope.pageSize;
+        return filteredBooks.slice(start, start + $scope.pageSize);
+    };
+
+    $scope.pageCount = function(filteredBooks) {
+        return Math.ceil(filteredBooks.length / $scope.pageSize) || 1;
+    };
+
+    $scope.setPage = function(page) {
+        $scope.currentPage = page;
+    };
+
+    // Inline edit
+    $scope.editBook = function(book) {
+        book.editing = true;
+        book._backup = angular.copy(book);
+    };
+
+    // Save after edit
+    $scope.saveBook = function(book) {
+        if (window.prodMode) {
+            $http.post("https://8ecbjv99ca.execute-api.ap-south-1.amazonaws.com/UAT/book", book)
+                .then(function(response) {
+                    book.editing = false;
+                    delete book._backup;
+                    $scope.loadBooks();
+                }, function(error) {
+                    alert("Failed to update book.");
+                });
+        } else {
+            book.editing = false;
+            delete book._backup;
+        }
+    };
+
+    // Cancel edit
+    $scope.cancelEdit = function(book) {
+        if (book._backup) {
+            angular.extend(book, book._backup);
+            delete book._backup;
+        }
+        book.editing = false;
+    };
+
+    // Modal variables
+    $scope.showModal = false;
+    $scope.newBook = { fundId: '', quantity: '', price: '', date: '' };
+
+    $scope.openModal = function() {
+        $scope.showModal = true;
+    };
+
+    $scope.closeModal = function() {
+        $scope.showModal = false;
+        $scope.newBook = { fundId: '', quantity: '', price: '', date: '' }; // Reset form
+    };
+
+    $scope.addBook = function() {
+        if (prodMode) {
+            $http.post('https://8ecbjv99ca.execute-api.ap-south-1.amazonaws.com/UAT/book', $scope.newBook)
+                .then(function(response) {
+                    $scope.books.push(response.data);
+                    $scope.closeModal();
+                })
+                .catch(function(error) {
+                    console.error('Error adding book:', error);
+                });
+        } else {
+            $scope.books.push(angular.copy($scope.newBook));
+            $scope.closeModal();
+        }
+    };
+
+    // Initial load
+    $scope.loadBooks();
+}]);
+
+app.controller('MaintainFundController', ['$scope', '$http', '$q', function($scope, $http, $q) {
+    $scope.funds = [];
+    $scope.newFund = {};
+
+    // Local fallback data for non-prodMode
+    var localFunds = [
+        { fundId: "F001", fundName: "Alpha Fund", fundType: "Equity", fundFamily: "Family A" },
+        { fundId: "F002", fundName: "Beta Fund", fundType: "Debt", fundFamily: "Family B" }
+    ];
+
+    // Fetch funds
+    $scope.loadFunds = function() {
+        if (window.prodMode) {
+            // Use $http for GET
+            $http.get("https://8ecbjv99ca.execute-api.ap-south-1.amazonaws.com/UAT/funds")
+                .then(function(response) {
+                    // If API returns { body: [...] }
+                    $scope.funds = response.data.body ? response.data.body : response.data;
+                }, function(error) {
+                    alert("Failed to load funds from API.");
+                });
+        } else {
+            // Use local data
+            $scope.funds = angular.copy(localFunds);
+        }
+    };
+
+    // Add a new fund
+    $scope.addFund = function() {
+        if (window.prodMode) {
+            $http.post("https://8ecbjv99ca.execute-api.ap-south-1.amazonaws.com/UAT/funds", $scope.newFund)
+                .then(function(response) {
+                    $scope.loadFunds();
+                    $scope.newFund = {};
+                }, function(error) {
+                    alert("Failed to add fund.");
+                });
+        } else {
+            $scope.funds.push(angular.copy($scope.newFund));
+            $scope.newFund = {};
+        }
+    };
+
+    // Edit fund (enable editing mode)
+    $scope.editFund = function(fund) {
+        fund.editing = true;
+        fund._backup = angular.copy(fund);
+    };
+
+    // Save fund (after editing)
+    $scope.saveFund = function(fund) {
+        if (window.prodMode) {
+            // For demo, just POST as add (real API should support PUT/PATCH)
+            $http.post("https://8ecbjv99ca.execute-api.ap-south-1.amazonaws.com/UAT/funds", fund)
+                .then(function(response) {
+                    fund.editing = false;
+                    delete fund._backup;
+                    $scope.loadFunds();
+                }, function(error) {
+                    alert("Failed to update fund.");
+                });
+        } else {
+            fund.editing = false;
+            delete fund._backup;
+        }
+    };
+
+    // Cancel editing
+    $scope.cancelEdit = function(fund) {
+        if (fund._backup) {
+            angular.extend(fund, fund._backup);
+            delete fund._backup;
+        }
+        fund.editing = false;
+    };
+
+    // Initial load
+    $scope.loadFunds();
+
+    $scope.showModal = false;
+
+    $scope.openModal = function() {
+        $scope.showModal = true;
+    };
+
+    $scope.closeModal = function() {
+        $scope.showModal = false;
+        $scope.newBook = { fundId: '', quantity: '', price: '', date: '' }; // Reset form
+    };
+
+
+}]);
